148c3e7a306ffab5af4a5830d66466468b1c3a01
AndreiDetenkov <a.detenkov@gmail.com>
2020-11-01 22:42:14 +0600

    style post screen, add remove handdler

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,13 +1,36 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, Button, ScrollView, Alert } from 'react-native';
+import { DATA } from '../data';
+import { THEME } from '../theme';
 
 export const PostScreen = ({ navigation }) => {
   const postId = navigation.getParam('postId');
 
+  const post = DATA.find((item) => item.id === postId);
+
+  const removeHandler = () => {
+    Alert.alert(
+      'Remove post',
+      'Are you sure?',
+      [
+        {
+          text: 'Cancel',
+          style: 'cancel',
+        },
+        { text: 'Remove', style: 'destructive', onPress: () => {} },
+      ],
+      { cancelable: false }
+    );
+  };
+
   return (
-    <View style={styles.center}>
-      <Text>{postId}</Text>
-    </View>
+    <ScrollView>
+      <Image source={{ uri: post.img }} style={styles.image} />
+      <View style={styles.textWrap}>
+        <Text style={styles.title}>{post.text}</Text>
+      </View>
+      <Button title="Remove" color={THEME.DANGER_COLOR} onPress={removeHandler} />
+    </ScrollView>
   );
 };
 
@@ -20,9 +43,15 @@ PostScreen.navigationOptions = ({ navigation }) => {
 };
 
 const styles = StyleSheet.create({
-  center: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+  image: {
+    width: '100%',
+    height: 200,
+  },
+  textWrap: {
+    padding: 10,
+  },
+  title: {
+    fontFamily: 'open-regular',
+    fontSize: 20,
   },
 });
