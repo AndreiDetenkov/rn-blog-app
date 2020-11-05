86efb38e2e5bb34424e9b5440b86937b4f2cdd95
AndreiDetenkov <a.detenkov@gmail.com>
2020-11-05 15:05:16 +0600

    refactor code to dispatches

--- src/screens/BookedScreen.js
+++ src/screens/BookedScreen.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { HeaderButtons, Item } from 'react-navigation-header-buttons'
-import { DATA } from '../data'
+import { useSelector } from 'react-redux'
 import { AppHeaderIcon } from '../components/AppHeaderIcon'
 import { PostList } from '../components/PostList'
 
@@ -9,7 +9,9 @@ export const BookedScreen = ({ navigation }) => {
     navigation.navigate('Post', { postId: post.id, date: post.date, booked: post.booked })
   }
 
-  return <PostList data={DATA.filter(post => post.booked)} onOpen={openPostHandler} />
+  const bookedPosts = useSelector(state => state.post.bookedPosts)
+
+  return <PostList data={bookedPosts} onOpen={openPostHandler} />
 }
 
 BookedScreen.navigationOptions = ({ navigation }) => ({
--- src/store/reducers/post.js
+++ src/store/reducers/post.js
@@ -1,5 +1,15 @@
-const initialState = {}
+import { LOAD_POSTS } from '../types'
+
+const initialState = {
+  allPosts: [],
+  bookedPosts: []
+}
 
 export const postReducer = (state = initialState, action) => {
-  return state
+  switch (action.type) {
+    case LOAD_POSTS:
+      return { ...state, allPosts: action.payload, bookedPosts: action.payload.filter(item => item.booked) }
+    default:
+      return state
+  }
 }
