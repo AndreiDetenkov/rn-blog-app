ab563685cb5d9114a801bdff5e6b9cb474882546
AndreiDetenkov <a.detenkov@gmail.com>
2020-11-05 14:22:21 +0600

    add redux, react-redux libs and reducer to project

--- App.js
+++ App.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
 import { AppLoading } from 'expo'
+import { StatusBar } from 'react-native'
+import { Provider } from 'react-redux'
 import { bootstrap } from './src/bootstrap'
 import { AppNavigator } from './src/navigation/AppNavigation'
-import { StatusBar } from 'react-native'
+import store from './src/store'
 
 export default function App() {
   const [isReady, setIsReady] = useState(false)
@@ -14,9 +16,9 @@ export default function App() {
   }
 
   return (
-    <>
+    <Provider store={store}>
       <StatusBar />
       <AppNavigator />
-    </>
+    </Provider>
   )
 }
