c6948a07d6574f7f0112dee63418f8cfc852a2e8
AndreiDetenkov <a.detenkov@gmail.com>
2020-11-05 15:49:27 +0600

    booked, removed posts

--- src/screens/PostScreen.js
+++ src/screens/PostScreen.js
@@ -1,13 +1,29 @@
-import React, { useEffect } from 'react';
-import { StyleSheet, Text, View, Image, Button, ScrollView, Alert } from 'react-native';
-import { HeaderButtons, Item } from 'react-navigation-header-buttons';
-import { DATA } from '../data';
-import { THEME } from '../theme';
-import { AppHeaderIcon } from '../components/AppHeaderIcon';
+import React, { useEffect, useCallback } from 'react'
+import { StyleSheet, Text, View, Image, Button, ScrollView, Alert } from 'react-native'
+import { HeaderButtons, Item } from 'react-navigation-header-buttons'
+import { useDispatch, useSelector } from 'react-redux'
+import { THEME } from '../theme'
+import { AppHeaderIcon } from '../components/AppHeaderIcon'
+import { removePost, toggleBooked } from '../store/actions/post'
 
 export const PostScreen = ({ navigation }) => {
-  const postId = navigation.getParam('postId');
-  const post = DATA.find((item) => item.id === postId);
+  const postId = navigation.getParam('postId')
+  const post = useSelector(state => state.post.allPosts.find(item => item.id === postId))
+  const booked = useSelector(state => state.post.bookedPosts.some(post => post.id === postId))
+
+  useEffect(() => {
+    navigation.setParams({ booked })
+  }, [booked])
+
+  const dispatch = useDispatch()
+
+  const toggleHandler = useCallback(() => {
+    dispatch(toggleBooked(postId))
+  }, [dispatch, postId])
+
+  useEffect(() => {
+    navigation.setParams({ toggleHandler })
+  }, [toggleHandler])
 
   const removeHandler = () => {
     Alert.alert(
@@ -16,13 +32,24 @@ export const PostScreen = ({ navigation }) => {
       [
         {
           text: 'Cancel',
-          style: 'cancel',
+          style: 'cancel'
         },
-        { text: 'Remove', style: 'destructive', onPress: () => {} },
+        {
+          text: 'Remove',
+          style: 'destructive',
+          onPress: () => {
+            navigation.navigate('Main')
+            dispatch(removePost(postId))
+          }
+        }
       ],
       { cancelable: false }
-    );
-  };
+    )
+  }
+
+  if (!post) {
+    return null
+  }
 
   return (
     <ScrollView>
@@ -32,33 +59,35 @@ export const PostScreen = ({ navigation }) => {
       </View>
       <Button title="Remove" color={THEME.DANGER_COLOR} onPress={removeHandler} />
     </ScrollView>
-  );
-};
+  )
+}
 
 PostScreen.navigationOptions = ({ navigation }) => {
-  const date = navigation.getParam('date');
-  const booked = navigation.getParam('booked');
-  const iconName = booked ? 'ios-star' : 'ios-star-outline';
+  const date = navigation.getParam('date')
+  const booked = navigation.getParam('booked')
+  const toggleHandler = navigation.getParam('toggleHandler')
+
+  const iconName = booked ? 'ios-star' : 'ios-star-outline'
   return {
     headerTitle: new Date(date).toLocaleDateString('ru-Ru'),
     headerRight: () => (
       <HeaderButtons HeaderButtonComponent={AppHeaderIcon}>
-        <Item title="Take photo" iconName={iconName} onPress={() => {}} />
+        <Item title="Take photo" iconName={iconName} onPress={toggleHandler} />
       </HeaderButtons>
-    ),
-  };
-};
+    )
+  }
+}
 
 const styles = StyleSheet.create({
   image: {
     width: '100%',
-    height: 200,
+    height: 200
   },
   textWrap: {
-    padding: 10,
+    padding: 10
   },
   title: {
     fontFamily: 'open-regular',
-    fontSize: 20,
-  },
-});
+    fontSize: 20
+  }
+})
--- src/store/reducers/post.js
+++ src/store/reducers/post.js
@@ -1,4 +1,4 @@
-import { LOAD_POSTS } from '../types'
+import { LOAD_POSTS, REMOVE_POST, TOGGLE_BOOKED } from '../types'
 
 const initialState = {
   allPosts: [],
@@ -9,6 +9,20 @@ export const postReducer = (state = initialState, action) => {
   switch (action.type) {
     case LOAD_POSTS:
       return { ...state, allPosts: action.payload, bookedPosts: action.payload.filter(item => item.booked) }
+    case TOGGLE_BOOKED:
+      const allPosts = state.allPosts.map(post => {
+        if (post.id === action.payload) {
+          post.booked = !post.booked
+        }
+        return post
+      })
+      return { ...state, allPosts, bookedPosts: allPosts.filter(item => item.booked) }
+    case REMOVE_POST:
+      return {
+        ...state,
+        allPosts: state.allPosts.filter(post => post.id !== action.payload),
+        bookedPosts: state.bookedPosts.filter(post => post.id !== action.payload)
+      }
     default:
       return state
   }
